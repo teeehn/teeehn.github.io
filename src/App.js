80cdb830cf25d5cf2c1d6878e9f83b69a97077a7
Tom Nicolosi <tom@teeehn.com>
2019-04-21 23:02:27 -0700

    changed text color. Corrected class rule.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = ({ name, ...props}) => {
     return (
-        <div className="container-fluid d-flex flex-column align-items-center justify-content-center h-100 text-white bg-gradient-dark">
+        <div className="container-fluid d-flex flex-column align-items-center justify-content-center h-100 text-light bg-gradient-dark">
             <h1>{ name }</h1>
             <p>{ props.message }</p>
         </div>
