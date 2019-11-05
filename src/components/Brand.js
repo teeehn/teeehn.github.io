9d2e4a2e89903fea0d64b9ccbec80c59435a1cc3
Thomas Nicolosi <Thomas.Nicolosi@panasonic.aero>
2019-11-04 16:23:42 -0800

    Removed the inner wrap on the navbar.

--- src/components/Brand.js
+++ src/components/Brand.js
@@ -3,7 +3,7 @@ import logo from "../assets/images/tn-logo-160x104.png";
 
 function Brand () {
     return (
-        <div className="brand p-0 h-100 py-2">
+        <div className="brand h-100 py-2">
             <img className="h-100" src={logo} alt="teeehn logo" />
         </div>
     );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,8 +2,8 @@ import React from "react";
 
 function Navbar ({ children }) {
     return (
-        <nav className="p-0">
-            <div className="tn__site-content--inner h-100">
+        <nav>
+            <div className="h-100">
                 <input id="main_menu" type="checkbox" className="show" />
                 <label htmlFor="main_menu" className="burger pseudo button">menu</label>
                 {
