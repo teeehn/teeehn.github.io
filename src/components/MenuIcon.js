152fef694669384fae7aad471d1d3074133d4658
Thomas Nicolosi <Thomas.Nicolosi@panasonic.aero>
2019-11-04 17:24:06 -0800

    Added rudimentary hamburger for main menu.

---
+++ src/components/MenuIcon.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function MenuIcon () {
+    return (
+        <label htmlFor="main_menu" className="burger pseudo button menu-icon">
+            <span />
+            <span />
+            <span />
+        </label>
+    );
+}
+
+export default MenuIcon;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,12 @@
 import React from "react";
+import MenuIcon from "./MenuIcon";
 
 function Navbar ({ children }) {
     return (
         <nav>
             <div className="h-100">
                 <input id="main_menu" type="checkbox" className="show" />
-                <label htmlFor="main_menu" className="burger pseudo button">menu</label>
+                <MenuIcon />
                 {
                     children
                 }
