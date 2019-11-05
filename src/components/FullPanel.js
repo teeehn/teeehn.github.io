7954b09124f7ea2afba189a4648b0dd470c0b55f
Thomas Nicolosi <Thomas.Nicolosi@panasonic.aero>
2019-11-05 10:22:09 -0800

    Fix top margin for first-child panel full.

--- src/components/FullPanel.js
+++ src/components/FullPanel.js
@@ -4,11 +4,7 @@ import default_src from "../assets/images/balloon-sunrise.jpg";
 function FullPanel ({ image_src = default_src }) {
     return (
         <div className="tn__panel--full" style={{
-            backgroundImage: `url(${image_src})`,
-            width: "100%",
-            paddingBottom: "50%",
-            backgroundSize: "cover",
-            backgroundPosition: "center"
+            backgroundImage: `url(${image_src})`
         }} >
         </div>
     );
