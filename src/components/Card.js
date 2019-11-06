757f1eb407d3bea4439ff1a4fbe39f65ff9f7b24
Tom Nicolosi <tom@teeehn.com>
2019-11-05 21:23:45 -0800

    Extracted out the test data. Revised the card compenents to act like columns. Revised inner wrapper to allow picnic grid gutters to work.

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,21 +1,28 @@
 import React from "react";
 
-function Card () {
+function Card ({
+    header_content = '',
+    body_content = '',
+    footer_content = ''
+}) {
     return (
-        <article className="card">
-            <header>
-                <h3>Card Title</h3>
-            </header>
-            <p>
-            Praesent hendrerit feugiat massa nec aliquam. Aenean vestibulum tincidunt risus in interdum. Sed aliquam ligula eu posuere vulputate. Morbi eget diam sapien. Pellentesque pellentesque fringilla ex, id porttitor velit semper vel. Nulla condimentum congue ultrices. Duis tincidunt leo orci, ut rutrum lectus posuere sit amet. In faucibus massa nec mauris mattis, id ornare ipsum faucibus. Suspendisse tristique iaculis cursus.
-            </p>
-            <footer>
-                <div  className="flex justify-content-between">
-                    <button>Button 1</button>
-                    <button>Button 2</button>
-                </div>
-            </footer>
-        </article>
+        <div className="card--wrapper">
+            <article className="card">
+                <header>
+                {
+                    header_content
+                }
+                </header>
+                {
+                    body_content
+                }
+                <footer>
+                {
+                    footer_content
+                }
+                </footer>
+            </article>
+        </div>
     );
 };
 
